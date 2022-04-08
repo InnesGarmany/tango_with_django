993c619ee6b409c49f2521db0b05b4c4d2c3ccb5
InnesGarmany <89853655+InnesGarmany@users.noreply.github.com>
2022-04-08 12:38:56 +0100

    Chapter 9 - passes tests

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,8 +6,8 @@ from rango.forms import CategoryForm,UserForm, UserProfileForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -64,6 +64,7 @@ def show_category(request, category_name_slug):
     #
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -86,7 +87,7 @@ def add_category(request):
     # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -211,3 +212,16 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html')
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+# Use the login_required() decorator to ensure only those logged in can
+# access the view.
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
